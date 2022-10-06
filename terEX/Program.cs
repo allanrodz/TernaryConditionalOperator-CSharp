045641439d161f4db067356e1d240d27d89f8279
Reception2 <Reception2@DUNLRECEPTILL>
2022-10-06 08:31:58 +0100

    Add project files.

---
+++ terEX/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "corkx";
+            int cost = 20;
+
+            string country = (city == "cork" ? "Ireland" : "Unknown" );
+
+            Console.WriteLine(country);
+
+            int price = (cost < 25 ? 20 : 30);
+            Console.WriteLine(price);
+
+            Console.ReadLine();
+
+        }
+    }
+}
